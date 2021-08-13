e7d792457218fcc694cf50ad25f76430b4ec7e8a
Dev2 <dev2@babylonsciences.com>
2021-08-13 23:45:56 +0200

    first commit

---
+++ Peter Answers/Scripts/peter.js
@@ -0,0 +1,42 @@
+﻿var app = angular.module('peterApp', []).
+    controller('peterController', function ($scope) {
+        var cheatState = false;
+        $scope.answer = '';
+        var i = 0;
+        var dummyText = 'Peter please answer my question';
+        $scope.petition = '';
+        $scope.changeText = function (keyPressed) {
+            if (keyPressed === '.')
+            {
+                event.preventDefault();
+                cheatState = !cheatState;
+            }
+            if (cheatState) {
+                event.preventDefault();
+                if (keyPressed !== '.') {
+                    $scope.answer += keyPressed;
+                }
+                $scope.petition += dummyText[i] !== undefined ? dummyText[i] : ' ';
+            }
+            if (!cheatState && keyPressed === '.') {
+                $scope.petition += dummyText[i] !== undefined ? dummyText[i] : ' ';
+            }
+            i++;
+        };
+
+        $scope.resetPeter = function () {
+            cheatState = false;
+            $scope.answer = '';
+            i = 0;
+            $scope.petition = '';
+            $scope.question = '';
+            $scope.showAnswer = false;
+        };
+
+        $scope.fireAnswer = function (keyPressed) {
+            if (keyPressed === '?') {
+                $scope.showAnswer = true;
+            }
+        };
+
+    });
